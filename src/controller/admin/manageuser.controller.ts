76b9ecccac722de2ef3b11081bdbd83d1c522905
vochihieu <chihieu8320@gmail.com>
2022-01-13 00:00:11 +0700

    send mail seller to bider admin

--- src/controller/admin/manageuser.controller.ts
+++ src/controller/admin/manageuser.controller.ts
@@ -1,6 +1,7 @@
 import userService from '../../service/user.service'
 import User from '../../model/user.model';
 import template from "../../email_template/template";
+import template_seller_bider from "../../email_template/seller_bider";
 import mailService from '../../service/mail.service'
 import mail from "../../mailer/mailer";
 
@@ -96,6 +97,33 @@ class ManageController
         } catch (error) {
             console.log(error);
             res.sendStatus(500);
+        }
+    }
+
+    async seller_to_bider(req: any, res: any){
+        try {
+            const id = req.params.userID;
+            const user = <any> await User.findByIdAndUpdate(id, req.body);
+            if(user){
+                const form = {
+                    name: user.name,
+                }
+                //create a template
+                const forgot_pwd_template = template_seller_bider.seller_bider(form)
+                //create option (sent to who ??)
+                const mail_options = mailService.mail_options(user.email, forgot_pwd_template, "Seller to Bider");
+                //conect mail server
+                const transporter = mail.connect()
+                //send mail
+                mailService.send_mail(transporter, mail_options);
+                res.json({status: 200})
+            }
+            else{
+                res.sendStatus(400)
+            }
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(500)
         }
     }
 }
---
+++ src/email_template/seller_bider.ts
@@ -0,0 +1,21 @@
+function seller_bider(form: any){
+    const data = `
+        Hello "${form.name}"!
+        Warning...
+        Your account has been becoming a bider
+        Regards,
+        #WNC
+        
+        ----
+        WNC - Support Team
+        Hotline: (+84)000 111 000
+        
+        Follow Us at:
+        - Facebook: https://facebook.com/groups/wnc
+        - Youtube: https://www.youtube.com/channel/wnc
+        - Instagram: https://instagram.com/wnc'
+        `
+        return data;
+    }
+
+export default {seller_bider}
